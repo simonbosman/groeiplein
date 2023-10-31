3cd40952262465bbc38f286566e6accdc054fe12
Simon Bosman <simonbosman@gmail.com>
2023-10-31 17:10:49 +0100

    Added domein en feedback to IT

---
+++ src/test/java/nl/speyk/feedback/FeedbackResourceTest.java
@@ -0,0 +1,30 @@
+package nl.speyk.feedback;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.contains;
+
+@QuarkusTest
+public class FeedbackResourceTest {
+
+    @ConfigProperty(name = "speyk.jwt")
+    String jwt;
+
+    @Test
+    public void shouldListFeedback() {
+        given().auth().preemptive().oauth2(jwt)
+                .when().get("/feedback")
+                .then().statusCode(200)
+                .and().body("id", contains(1))
+                .and().body("timestamp", contains("2022-03-10T16:15:50Z"))
+                .and().body("updateTimestamp", contains("2022-03-10T16:15:50Z"))
+                .and().body("authorUuid", contains("3fa85f64-5717-4562-b3fc-2c963f66afa6"))
+                .and().body("content", contains("testcontent"))
+                .and().body("author", contains("DOCENT"))
+                .and().body("title", contains("testtitle"));
+    }
+}
