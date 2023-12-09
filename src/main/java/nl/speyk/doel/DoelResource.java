f5bb14f96c8a91cea8b1cfb7a72a215b6918f85c
Simon Bosman <simonbosman@gmail.com>
2023-12-09 20:48:54 +0100

    Fixed rbac for endopoint DoelResource

--- src/main/java/nl/speyk/doel/DoelResource.java
+++ src/main/java/nl/speyk/doel/DoelResource.java
@@ -2,8 +2,17 @@ package nl.speyk.doel;
 
 import io.quarkus.hibernate.reactive.rest.data.panache.PanacheRepositoryResource;
 import io.quarkus.rest.data.panache.ResourceProperties;
-
+import io.smallrye.mutiny.Uni;
+import jakarta.annotation.security.RolesAllowed;
 
 @ResourceProperties(rolesAllowed = "**")
 public interface DoelResource extends PanacheRepositoryResource<DoelRepository, Doel, Long> {
+    @RolesAllowed("${speyk.roles.docent}")
+    Uni<Doel> add(Doel entity);
+
+    @RolesAllowed("${speyk.roles.docent}")
+    Uni<Doel> update(Long id, Doel entity);
+
+    @RolesAllowed("${speyk.roles.docent}")
+    Uni<Boolean> delete(Long id);
 }
