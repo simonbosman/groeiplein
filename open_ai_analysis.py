a46980c9ce397d737042b0eec5afa02cd22ba13e
Simon Bosman <simonbosman@gmail.com>
2024-02-27 15:57:10 +0100

    Added some AI poc's

---
+++ open_ai_analysis.py
@@ -0,0 +1,53 @@
+import warnings;
+from langchain.text_splitter import Language
+from langchain_community.document_loaders.generic import GenericLoader
+from langchain_community.document_loaders.parsers import LanguageParser
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationSummaryMemory
+from langchain_openai import ChatOpenAI
+
+warnings.filterwarnings("ignore")
+repo_path = "/Users/simon/digitaal-portfolio"
+
+# Load
+loader = GenericLoader.from_filesystem(
+    repo_path + "/src/main/java/nl/speyk",
+    glob="**/*",
+    suffixes=[".java"],
+    parser=LanguageParser(language=Language.JAVA, parser_threshold=500),
+)
+documents = loader.load()
+
+# Load from path /src/main/java/nl/speyk
+
+java_splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.JAVA, chunk_size=2000, chunk_overlap=200
+)
+texts = java_splitter.split_documents(documents)
+
+db = Chroma.from_documents(texts, OpenAIEmbeddings(disallowed_special=()))
+retriever = db.as_retriever(
+    search_type="mmr",  # Also test "similarity"
+    search_kwargs={"k": 8},
+)
+
+llm = ChatOpenAI(model="gpt-4")
+memory = ConversationSummaryMemory(
+    llm=llm, memory_key="chat_history", return_messages=True
+)
+qa = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
+
+uestions = [
+    "Kun je uitleggen wat de code doet?",
+    "Wat kun je over doel vertellen?",
+    "Zou je een test voor Doel kunnen schrijven?",
+]
+
+for question in questions:
+    result = qa(question)
+    print(f"-> **Question**: {question} \n")
+    print(f"**Answer**: {result['answer']} \n")
